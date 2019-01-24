f059d2ebb188d3357ab919260fa49f9e2b1dd3dc
lucha93 <drew.adamski@gmail.com>
2019-01-23 19:09:29 -0800

    Formatting changes

--- climbing.py
+++ climbing.py
@@ -10,11 +10,12 @@ climbing_dates = []
 for date in sheet['Date'].tolist():
     climbing_dates.append(datetime.strptime(date, '%Y-%m-%d'))
 
+    
 # Make 2018_days list with all dates of 2018
-
 date1 = '1-1-2018'
 date2 = '12-31-2018'
 year_dates = pd.date_range(date1, date2).tolist()
+
 
 # Make list of cumulative climb count
 climb_count = []
@@ -24,17 +25,13 @@ for date in year_dates:
         total +=1
         climb_count.append(total)
     else:
-        climb_count.append(total)
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-         ]
-
-
+        climb_count.append(total)  
+        
+        
 # Graph cumulative climb count
 plt.plot(year_dates, climb_count, label = 'My Progress')
 plt.plot([year_dates[0], year_dates[-1]], [0, 100], 
          label = 'Climbing Rate to Realize Goal', color = 'red')
-
 plt.axvline(datetime.strptime('2018-04-28', '%Y-%m-%d'), dashes=[1,1], 
             label = '4/28/18-5/8/18 Korea Trip', color ='green')
 plt.axvline(datetime.strptime('2018-05-8', '%Y-%m-%d'), dashes=[1,1],
@@ -49,6 +46,7 @@ plt.title('Climbing 100x in 2018')
 plt.legend()
 plt.show()
 
+
 # Sum the times I went climbing by month, and show it as a bar graph
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
           'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
@@ -60,6 +58,7 @@ totals = []
 for i in range(1, 13):
     totals.append(events.count(i))
 
+# Graph histogram of monthly climbing totals
 plt.bar(range(1,13), totals, tick_label = months, color='blue')
 plt.title('Monthly Climbing Totals')
 plt.xlabel('Month')
